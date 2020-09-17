5ad501836bac7eb18351ee9db77149c8c021cfc1
haluto <haluto8086@sina.cn>
2020-09-17 17:13:40 +0800

    change the structure of the webapp.

---
+++ src/components/common/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+
+export default class App extends Component {
+    
+  render() {
+    return (
+      <div>Main Page</div>
+    );
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../components/common/history';
+
+import App from '../components/common/App';
+import NoMatch from '../components/common/404';
+
+class MRoute extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={App}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default MRoute;

5cea8e11259cf87ba1eae1da4c884ba4fd8a7015
haluto <haluto8086@sina.cn>
2020-09-18 10:56:31 +0800

    webapp: use antd and use App as route.

--- src/components/common/App.js
+++ src/components/common/App.js
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
 
+import BasicInfoScreen from './BasicInfoScreen';
+
+
 export default class App extends Component {
     
   render() {
     return (
-      <div>Main Page</div>
+      <div>
+        <BasicInfoScreen />
+      </div>
     );
   }
 }
---
+++ src/components/common/BasicInfoScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Button } from 'antd';
+
+export default class BasicInfoScreen extends Component {
+
+  render() {
+    return (
+      <div>
+        BasicInfoScreen
+        <Button type="primary">ClickMe</Button>
+      </div>
+    );
+  }
+}

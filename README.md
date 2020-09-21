9c948ae311a8041d19d8c3b1f7edb098b9d9ed85
haluto <haluto8086@sina.cn>
2020-09-21 17:43:27 +0800

    Device Choose Panel

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # debugUI
+
+# Development Notes
+
+### Icon of antd
+
+- antd中的Icon需要额外安装：
+
+```shell
+$ npm install --save @ant-design/icons
+```
+
+- 安装后使用时不再是`<Icon type="xxx" />`，而是每个icon都直接用自己的component
+
+  ```javascript
+  import { MobileOutlined } from '@ant-design/icons';
+  
+  ...
+  
+  <MobileOutlined />
+  ```
+
+  
+
--- src/components/BasicInfoScreen/DeviceChooserPanel.js
+++ src/components/BasicInfoScreen/DeviceChooserPanel.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Select, Button } from 'antd';
+import {ReloadOutlined} from '@ant-design/icons';
 import $ from 'jquery';
 
 export default class DeviceChooserPanel extends React.Component {
   state = {
-    devices: []
+    devices: [],
+    selectedDevice: ''
   };
 
   getDevices = () => {
@@ -28,14 +31,52 @@ export default class DeviceChooserPanel extends React.Component {
     });
   }
 
+  handleDeviceSelectChange = (value) => {
+    if (value == "empty") {
+      alert("Error, no device connected but selected the empty, should not happen.");
+      return;
+    }
+
+    this.setState({
+      selectedDevice: value
+    });
+  }
+
+  handleReloadButtonClick = () => {
+    this.getDevices();
+  }
+
   componentDidMount = () => {
     this.getDevices();
   }
 
   render() {
+    let items = [];
+    if (this.state.devices.length == 0) {
+      items.push(
+        <Select.Option value="empty" disabled style={{ width: 180 }}>No device</Select.Option>
+      );
+    } else {
+      this.state.devices.map((device, i) => {
+        items.push(
+        <Select.Option value={device} style={{ width: 180 }}>{device}</Select.Option>
+        );
+      });
+    }
+
     return (
-      <div>
-        {this.state.devices.length == 0 ? "<No Device>":this.state.devices}
+      <div className="divice-choose-panel">
+        <Button type="text" shape="circle" className="reload-button"
+                icon={<ReloadOutlined />} 
+                onClick={this.handleReloadButtonClick}/>
+
+        <Select defaultValue="Choose a device:" onChange={this.handleDeviceSelectChange} style={{ width: 180 }}>
+          {items}
+        </Select>
+        
+        <div className="connected-device-info">
+          已连接设备：{this.state.selectedDevice}
+        </div>
       </div>
     );
   }
